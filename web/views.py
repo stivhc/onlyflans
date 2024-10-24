9e2ff75d7f126856dc1342e113c0455c4dcb5669
stiv.hc <esteban.alfonso999@gmail.com>
2024-10-23 21:30:58 -0300

    Views Web

--- web/views.py
+++ web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def indice(request):
+    return render(request, "index.html", {})
+
+
+def acerca(request):
+    return render(request, "about.html", {})
+
+
+def bienvenido(request):
+    return render(request, "welcome.html", {})
