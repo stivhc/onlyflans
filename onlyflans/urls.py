d1cc7416820cbb7b33cb2225dfb1810f29e87a62
stiv.hc <esteban.alfonso999@gmail.com>
2024-10-23 21:42:24 -0300

    urls web

--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from web.views import indice, acerca, bienvenido
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", indice, name="acerca"),
+    path("acerca", acerca, name="acerca"),
+    path("bienvenido", bienvenido, name="bienvenido"),
 ]
